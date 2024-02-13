ce7d95e4f853a421c767bd6ac5395ffff06a3102
Ala Eddine Khiari <135550744+AlaEddine-Khiari@users.noreply.github.com>
2024-02-13 14:06:39 +0100

    Update image_test.py

--- Test/image_test.py
+++ Test/image_test.py
@@ -1,35 +1,33 @@
 import docker
 
-def run_sip_module_test(image_name):
+def verify_sip_module(image_name):
     # Connect to Docker daemon
     client = docker.from_env()
 
     try:
         # Run the Docker container
-        container = client.containers.run(image_name, detach=True)
+        container = client.containers.run(image_name, detach=True, tty=True)
 
-        # Execute SIP-related test commands within the container
-        test_command = "asterisk -rx 'sip show peers'"
-        exec_result = container.exec_run(test_command)
+        # Execute Asterisk CLI command to check if SIP module is loaded
+        exec_command = "asterisk -rx 'module show like chan_sip.so'"
+        exec_result = container.exec_run(exec_command)
 
-        # Print test command output
-        print("SIP Module Test Output:")
+        # Print command output
+        print("Command Output:")
         print(exec_result.output.decode())
 
-        # Check if test output indicates success or failure
-        if "Some Success Condition" in exec_result.output.decode():
-            print("SIP module test passed!")
+        # Check if SIP module is loaded
+        if "chan_sip.so" in exec_result.output.decode():
+            print("SIP module is loaded!")
         else:
-            print("SIP module test failed!")
-            # Exit
-            exit(1)
+            print("SIP module is not loaded!")
 
     finally:
-        #Delete the container
+        # Delete the container
         if container:
             container.remove(force=True)
             print("Container deleted successfully.")
 
 if __name__ == "__main__":
     # Replace "your-asterisk-image" with the name of your Asterisk Docker image
-    run_sip_module_test("asterisk-image")
+    verify_sip_module("asterisk-image")

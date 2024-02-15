a079607c038bec58ed8e85192aa91317aeada928
Ala Eddine Khiari <135550744+AlaEddine-Khiari@users.noreply.github.com>
2024-02-15 21:37:10 +0100

    Update image_test.sh

--- Test/image_test.sh
+++ Test/image_test.sh
@@ -2,8 +2,8 @@
 
 # Start Asterisk Docker container
 docker run -d --name asterisk-cli \
-    -v sip.conf:/etc/asterisk/sip.conf \
-    -v voicemail.conf:/etc/asterisk/voicemail.conf \
+    -v ./sip.conf:/etc/asterisk/sip.conf \
+    -v ./voicemail.conf:/etc/asterisk/voicemail.conf \
     asterisk-image
 
 # Check if the container started successfully
